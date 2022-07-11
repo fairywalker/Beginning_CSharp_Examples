fe145f7ac05877eea9017350a9e8c305e76c67eb
fairywalker <tema-1s@yandex.ru>
2022-07-11 16:56:44 +0300

    ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int index = 0;
+
+        while (index < count)
+    {
+        Console.WriteLine(col[index]);
+        index++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index+1;
+            break;
+        }
+        index++;
+}
+return position;
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+Console.Write("Position: ");
+Console.Write(IndexOf(array, 2));
